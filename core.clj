be03bbdb2b0781c02702b6c839b7113d9081cc67
Roman Bataev <roman.bataev@gmail.com>
2016-05-19 22:15:25 -0700

    core: when

--- core.clj
+++ core.clj
@@ -341,3 +341,9 @@
                      (list 'var name)))))
 
 (set-macro* #'defmacro)
+
+(defmacro when
+  "Evaluates test. If logical true, evaluates body in an implicit do."
+  {:added "1.0"}
+  [test & body]
+  (list 'if test (cons 'do body)))
