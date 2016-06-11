6dd9a33234ac931f6f49460588917797931dd0c6
Roman Bataev <roman.bataev@gmail.com>
2016-06-11 13:21:12 -0700

    core: or

--- data/core.clj
+++ data/core.clj
@@ -589,3 +589,15 @@
   ([x & next]
    `(let [and# ~x]
       (if and# (and ~@next) and#))))
+
+(defmacro or
+  "Evaluates exprs one at a time, from left to right. If a form
+  returns a logical true value, or returns that value and doesn't
+  evaluate any of the other expressions, otherwise it returns the
+  value of the last expression. (or) returns nil."
+  {:added "1.0"}
+  ([] nil)
+  ([x] x)
+  ([x & next]
+      `(let [or# ~x]
+         (if or# or# (or ~@next)))))
