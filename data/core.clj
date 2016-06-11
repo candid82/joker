da82ec273974311d86591234efd04d6accc99ae6
Roman Bataev <roman.bataev@gmail.com>
2016-06-11 13:20:30 -0700

    core: and

--- data/core.clj
+++ data/core.clj
@@ -577,3 +577,15 @@
   must implement Comparable"
   {:added "1.0"}
   [x y] (compare* x y))
+
+(defmacro and
+  "Evaluates exprs one at a time, from left to right. If a form
+  returns logical false (nil or false), and returns that value and
+  doesn't evaluate any of the other expressions, otherwise it returns
+  the value of the last expr. (and) returns true."
+  {:added "1.0"}
+  ([] true)
+  ([x] x)
+  ([x & next]
+   `(let [and# ~x]
+      (if and# (and ~@next) and#))))
