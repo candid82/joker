397b881fc0dc08efcef23e794f32391f84afa132
Roman Bataev <roman.bataev@gmail.com>
2016-06-26 21:26:56 -0700

    core: ->>

--- data/core.clj
+++ data/core.clj
@@ -1086,3 +1086,19 @@
                        (list form x))]
         (recur threaded (next forms)))
       x)))
+
+(defmacro ->>
+  "Threads the expr through the forms. Inserts x as the
+  last item in the first form, making a list of it if it is not a
+  list already. If there are more forms, inserts the first form as the
+  last item in second form, etc."
+  {:added "1.0"}
+  [x & forms]
+  (loop [x x, forms forms]
+    (if forms
+      (let [form (first forms)
+            threaded (if (seq? form)
+                       (with-meta `(~(first form) ~@(next form)  ~x) (meta form))
+                       (list form x))]
+        (recur threaded (next forms)))
+      x)))
