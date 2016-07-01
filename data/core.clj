cf9e609ead79db73cf12e08293a7c0b6d697d444
Roman Bataev <roman.bataev@gmail.com>
2016-06-30 22:44:04 -0700

    core: if-some

--- data/core.clj
+++ data/core.clj
@@ -1147,3 +1147,23 @@
        (when temp#
          (let [~form temp#]
            ~@body)))))
+
+(defmacro if-some
+  "bindings => binding-form test
+
+  If test is not nil, evaluates then with binding-form bound to the
+  value of test, if not, yields else"
+  {:added "1.0"}
+  ([bindings then]
+   `(if-some ~bindings ~then nil))
+  ([bindings then else & oldform]
+   (assert-args
+    (vector? bindings) "a vector for its binding"
+    (nil? oldform) "1 or 2 forms after binding vector"
+    (= 2 (count bindings)) "exactly 2 forms in binding vector")
+   (let [form (bindings 0) tst (bindings 1)]
+     `(let [temp# ~tst]
+        (if (nil? temp#)
+          ~else
+          (let [~form temp#]
+            ~then))))))
