9663eb085680e8be02c1e29893beba39ec6300cf
Roman Bataev <roman.bataev@gmail.com>
2016-06-30 22:41:18 -0700

    core: when-let

--- data/core.clj
+++ data/core.clj
@@ -1132,3 +1132,18 @@
           (let [~form temp#]
             ~then)
           ~else)))))
+
+(defmacro when-let
+  "bindings => binding-form test
+
+  When test is true, evaluates body with binding-form bound to the value of test"
+  {:added "1.0"}
+  [bindings & body]
+  (assert-args
+   (vector? bindings) "a vector for its binding"
+   (= 2 (count bindings)) "exactly 2 forms in binding vector")
+  (let [form (bindings 0) tst (bindings 1)]
+    `(let [temp# ~tst]
+       (when temp#
+         (let [~form temp#]
+           ~@body)))))
