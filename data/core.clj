3f6e0b600985ee1b5329e4899f2503a82058bc73
Roman Bataev <roman.bataev@gmail.com>
2016-07-01 22:13:12 -0700

    core: comp

--- data/core.clj
+++ data/core.clj
@@ -1185,9 +1185,38 @@
          (let [~form temp#]
            ~@body)))))
 
-
 (defn find-var
   "Returns the global var named by the namespace-qualified symbol, or
   nil if no var with that name."
   {:added "1.0"}
   [sym] (find-var* sym))
+
+(defn comp
+  "Takes a set of functions and returns a fn that is the composition
+  of those fns.  The returned fn takes a variable number of args,
+  applies the rightmost of fns to the args, the next
+  fn (right-to-left) to the result, etc."
+  {:added "1.0"}
+  ([] identity)
+  ([f] f)
+  ([f g]
+   (fn
+     ([] (f (g)))
+     ([x] (f (g x)))
+     ([x y] (f (g x y)))
+     ([x y z] (f (g x y z)))
+     ([x y z & args] (f (apply g x y z args)))))
+  ([f g h]
+   (fn
+     ([] (f (g (h))))
+     ([x] (f (g (h x))))
+     ([x y] (f (g (h x y))))
+     ([x y z] (f (g (h x y z))))
+     ([x y z & args] (f (g (apply h x y z args))))))
+  ([f1 f2 f3 & fs]
+   (let [fs (reverse (list* f1 f2 f3 fs))]
+     (fn [& args]
+       (loop [ret (apply (first fs) args) fs (next fs)]
+         (if fs
+           (recur ((first fs) ret) (next fs))
+           ret))))))
