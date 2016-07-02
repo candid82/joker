f5db80effc172490769827206efbf764a3917268
Roman Bataev <roman.bataev@gmail.com>
2016-07-01 22:16:23 -0700

    core: partial

--- data/core.clj
+++ data/core.clj
@@ -1257,3 +1257,18 @@
        ([x y] (reduce1 #(conj %1 (%2 x y)) [] fs))
        ([x y z] (reduce1 #(conj %1 (%2 x y z)) [] fs))
        ([x y z & args] (reduce1 #(conj %1 (apply %2 x y z args)) [] fs))))))
+
+(defn partial
+  "Takes a function f and fewer than the normal arguments to f, and
+  returns a fn that takes a variable number of additional args. When
+  called, the returned function calls f with args + additional args."
+  {:added "1.0"}
+  ([f] f)
+  ([f arg1]
+   (fn [& args] (apply f arg1 args)))
+  ([f arg1 arg2]
+   (fn [& args] (apply f arg1 arg2 args)))
+  ([f arg1 arg2 arg3]
+   (fn [& args] (apply f arg1 arg2 arg3 args)))
+  ([f arg1 arg2 arg3 & more]
+   (fn [& args] (apply f arg1 arg2 arg3 (concat more args)))))
