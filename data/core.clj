5cef308c20a8af694138096847325da84cfa5057
Roman Bataev <roman.bataev@gmail.com>
2016-07-01 22:25:32 -0700

    core: some

--- data/core.clj
+++ data/core.clj
@@ -1300,3 +1300,13 @@
          :arglists '([pred coll])
          :added "1.0"}
   not-every? (comp not every?))
+
+(defn some
+  "Returns the first logical true value of (pred x) for any x in coll,
+  else nil.  One common idiom is to use a set as pred, for example
+  this will return :fred if :fred is in the sequence, otherwise nil:
+  (some #{:fred} coll)"
+  {:added "1.0"}
+  [pred coll]
+  (when (seq coll)
+    (or (pred (first coll)) (recur pred (next coll)))))
