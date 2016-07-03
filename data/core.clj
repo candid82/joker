9b687d5f521fed7401b42dc82a0d72ef2888b248
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:43:31 -0700

    core: drop

--- data/core.clj
+++ data/core.clj
@@ -1411,3 +1411,14 @@
    (when-let [s (seq coll)]
      (when (pred (first s))
        (cons (first s) (take-while pred (rest s)))))))
+
+(defn drop
+  "Returns a lazy sequence of all but the first n items in coll."
+  {:added "1.0"}
+  [n coll]
+  (let [step (fn [n coll]
+               (let [s (seq coll)]
+                 (if (and (pos? n) s)
+                   (recur (dec n) (rest s))
+                   s)))]
+    (lazy-seq (step n coll))))
