73276328bd954b690232daea52df3072403ab6f9
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:57:10 -0700

    core: drop-while

--- data/core.clj
+++ data/core.clj
@@ -1438,3 +1438,15 @@
     (if lead
       (recur (next s) (next lead))
       s)))
+
+(defn drop-while
+  "Returns a lazy sequence of the items in coll starting from the first
+  item for which (pred item) returns logical false."
+  {:added "1.0"}
+  [pred coll]
+  (let [step (fn [pred coll]
+               (let [s (seq coll)]
+                 (if (and s (pred (first s)))
+                   (recur pred (rest s))
+                   s)))]
+    (lazy-seq (step pred coll))))
