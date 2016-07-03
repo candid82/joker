e7dbf23671b7a91251a52f5cd1a62faae7e97e64
Roman Bataev <roman.bataev@gmail.com>
2016-07-03 16:40:37 -0700

    core: declare

--- data/core.clj
+++ data/core.clj
@@ -1538,3 +1538,8 @@
              (next ks)
              (next vs))
       map)))
+
+(defmacro declare
+  "defs the supplied var names with no bindings, useful for making forward declarations."
+  {:added "1.0"}
+  [& names] `(do ~@(map #(list 'def (vary-meta % assoc :declared true)) names)))
