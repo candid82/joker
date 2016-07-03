07bed582f9e4164de192ad7d090007605766f3c6
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:37:36 -0700

    core: take

--- data/core.clj
+++ data/core.clj
@@ -1391,3 +1391,13 @@
   {:added "1.0"}
   [pred coll]
   (filter (complement pred) coll))
+
+(defn take
+  "Returns a lazy sequence of the first n items in coll, or all items if
+  there are fewer than n."
+  {:added "1.0"}
+  [n coll]
+  (lazy-seq
+   (when (pos? n)
+     (when-let [s (seq coll)]
+       (cons (first s) (take (dec n) (rest s)))))))
