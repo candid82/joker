62840669e30a67a167769d0b56caca986a5be96a
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:46:24 -0700

    core: drop-last

--- data/core.clj
+++ data/core.clj
@@ -1422,3 +1422,9 @@
                    (recur (dec n) (rest s))
                    s)))]
     (lazy-seq (step n coll))))
+
+(defn drop-last
+  "Return a lazy sequence of all but the last n (default 1) items in coll"
+  {:added "1.0"}
+  ([s] (drop-last 1 s))
+  ([n s] (map (fn [x _] x) s (drop n s))))
