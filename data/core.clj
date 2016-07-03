ab37b1fdd1a3ca6959b985525f94e70171ef6c0f
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:41:00 -0700

    core: take-while

--- data/core.clj
+++ data/core.clj
@@ -1401,3 +1401,13 @@
    (when (pos? n)
      (when-let [s (seq coll)]
        (cons (first s) (take (dec n) (rest s)))))))
+
+(defn take-while
+  "Returns a lazy sequence of successive items from coll while
+  (pred item) returns true. pred must be free of side-effects."
+  {:added "1.0"}
+  [pred coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (when (pred (first s))
+       (cons (first s) (take-while pred (rest s)))))))
