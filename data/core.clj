ca2b6c839d21500d87053e49dc94abf0950f2009
Roman Bataev <roman.bataev@gmail.com>
2016-07-02 21:58:49 -0700

    core: cycle

--- data/core.clj
+++ data/core.clj
@@ -1450,3 +1450,10 @@
                    (recur pred (rest s))
                    s)))]
     (lazy-seq (step pred coll))))
+
+(defn cycle
+  "Returns a lazy (infinite!) sequence of repetitions of the items in coll."
+  {:added "1.0"}
+  [coll] (lazy-seq
+          (when-let [s (seq coll)]
+            (concat s (cycle s)))))
