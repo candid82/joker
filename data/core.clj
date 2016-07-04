bd13fd90cb149facde5ced1d06318cb50add9bcf
Roman Bataev <roman.bataev@gmail.com>
2016-07-03 22:32:22 -0700

    core: dorun

--- data/core.clj
+++ data/core.clj
@@ -1562,3 +1562,17 @@
    (sort-by keyfn compare coll))
   ([keyfn ^Comparator comp coll]
    (sort (fn [x y] (comp (keyfn x) (keyfn y))) coll)))
+
+(defn dorun
+  "When lazy sequences are produced via functions that have side
+  effects, any effects other than those needed to produce the first
+  element in the seq do not occur until the seq is consumed. dorun can
+  be used to force any effects. Walks through the successive nexts of
+  the seq, does not retain the head and returns nil."
+  {:added "1.0"}
+  ([coll]
+   (when (seq coll)
+     (recur (next coll))))
+  ([n coll]
+   (when (and (seq coll) (pos? n))
+     (recur (dec n) (next coll)))))
