f36258c21a13d043ab61c37f9a51639e619d5347
Roman Bataev <roman.bataev@gmail.com>
2016-07-03 22:35:28 -0700

    core: doall

--- data/core.clj
+++ data/core.clj
@@ -1576,3 +1576,18 @@
   ([n coll]
    (when (and (seq coll) (pos? n))
      (recur (dec n) (next coll)))))
+
+(defn doall
+  "When lazy sequences are produced via functions that have side
+  effects, any effects other than those needed to produce the first
+  element in the seq do not occur until the seq is consumed. doall can
+  be used to force any effects. Walks through the successive nexts of
+  the seq, retains the head and returns it, thus causing the entire
+  seq to reside in memory at one time."
+  {:added "1.0"}
+  ([coll]
+   (dorun coll)
+   coll)
+  ([n coll]
+   (dorun n coll)
+   coll))
