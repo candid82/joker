7e13d07a917701ebb0075139cffd3157155aac34
Roman Bataev <roman.bataev@gmail.com>
2016-08-18 21:21:27 -0700

    Fix gen_types

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # go-bindata -o core/bindata.go data
-# go generate &&
-go tool vet ./ && go build && ./gclojure $@
+go generate && go tool vet ./ && go build && ./gclojure $@
