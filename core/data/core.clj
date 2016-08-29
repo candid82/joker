0a728cb4711d6b3f6b7480e04fc358edd29a2036
Roman Bataev <roman.bataev@gmail.com>
2016-08-28 17:09:04 -0700

    core: lazy-cat

--- core/data/core.clj
+++ core/data/core.clj
@@ -2315,6 +2315,15 @@
        (let [~x (first xs#)]
          ~@body))))
 
+(defmacro lazy-cat
+  "Expands to code which yields a lazy sequence of the concatenation
+  of the supplied colls.  Each coll expr is not evaluated until it is
+  needed.
+
+  (lazy-cat xs ys zs) === (concat (lazy-seq xs) (lazy-seq ys) (lazy-seq zs))"
+  {:added "1.0"}
+  [& colls]
+  `(concat ~@(map #(list `lazy-seq %) colls)))
 
 (defmacro assert
   "Evaluates expr and throws an exception if it does not evaluate to
