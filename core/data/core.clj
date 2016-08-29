cbb52bd9f5c9175e0fe0f4d98a84acbdb4dcec05
Roman Bataev <roman.bataev@gmail.com>
2016-08-28 17:06:38 -0700

    core: when-first

--- core/data/core.clj
+++ core/data/core.clj
@@ -2301,7 +2301,19 @@
                   (let ~(vec (interleave bs gs))
                     ~@body)))))))
 
+(defmacro when-first
+  "bindings => x xs
 
+  Roughly the same as (when (seq xs) (let [x (first xs)] body)) but xs is evaluated only once"
+  {:added "1.0"}
+  [bindings & body]
+  (assert-args
+   (vector? bindings) "a vector for its binding"
+   (= 2 (count bindings)) "exactly 2 forms in binding vector")
+  (let [[x xs] bindings]
+    `(when-let [xs# (seq ~xs)]
+       (let [~x (first xs#)]
+         ~@body))))
 
 
 (defmacro assert
