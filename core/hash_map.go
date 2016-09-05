080fd781518b81bbde54c1ae723eafd8338f90d9
Roman Bataev <roman.bataev@gmail.com>
2016-09-04 22:33:04 -0700

    HashMap

--- core/hash_map.go
+++ core/hash_map.go
@@ -16,11 +16,62 @@ type (
 	HashMap struct {
 		InfoHolder
 		MetaHolder
-		count     int
-		root      Node
-		hasNull   bool
-		nullValue Object
+		count int
+		root  Node
+	}
+	BitmapIndexedNode struct {
+		bitmap int
+		array  []Object
 	}
 )
 
-var EmptyHashMap = &HashMap{}
+var (
+	EmptyHashMap     = &HashMap{}
+	emptyIndexedNode = &BitmapIndexedNode{}
+	notFound         = EmptyArrayMap()
+)
+
+func bitCount(n int) int {
+	var count int
+	for n != 0 {
+		count++
+		n &= n - 1
+	}
+	return count
+}
+
+func (b *BitmapIndexedNode) index(bit int) int {
+	return bitCount(b.bitmap & (bit - 1))
+}
+
+func (m *HashMap) containsKey(key Object) bool {
+	if m.root != nil {
+		return m.root.tryFind(0, int(key.Hash()), key, notFound) != notFound
+	} else {
+		return false
+	}
+}
+
+// func (m *HashMap) Assoc(key, val Object) Associative {
+// 	addedLeaf := &Box{}
+// 	var newroot, t Node
+// 	if m.root == nil {
+// 		t = EmptyBitmapIndexedNode
+// 	} else {
+// 		t = root
+// 	}
+// 	newroot = t.assoc(0, key.Hash(), key, val, addedLeaf)
+// 	if newroot == root {
+// 		return m
+// 	}
+// 	newcount := m.count
+// 	if addedLeaf.val != nil {
+// 		newcount = m.count + 1
+// 	}
+// 	return &HashMap{
+// 		count: newcount,
+// 		root:  newroot,
+// 		meta:  m.meta,
+// 	}
+
+// }
