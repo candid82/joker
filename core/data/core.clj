6f52c14e791324e422aef482fa3594458feb6c3d
Roman Bataev <roman.bataev@gmail.com>
2016-10-05 21:40:30 -0700

    core: binding

--- core/data/core.clj
+++ core/data/core.clj
@@ -2419,6 +2419,27 @@
   [binding-map & body]
   `(with-bindings* ~binding-map (fn [] ~@body)))
 
+(defmacro binding
+  "binding => var-symbol init-expr
+
+  Creates new bindings for the (already-existing) vars, with the
+  supplied initial values, executes the exprs in an implicit do, then
+  re-establishes the bindings that existed before.  The new bindings
+  are made in parallel (unlike let); all init-exprs are evaluated
+  before the vars are bound to their new values."
+  {:added "1.0"}
+  [bindings & body]
+  (assert-args
+    (vector? bindings) "a vector for its binding"
+    (even? (count bindings)) "an even number of forms in binding vector")
+  (let [var-ize (fn [var-vals]
+                  (loop [ret [] vvs (seq var-vals)]
+                    (if vvs
+                      (recur  (conj (conj ret `(var ~(first vvs))) (second vvs))
+                             (next (next vvs)))
+                      (seq ret))))]
+    `(with-bindings (hash-map ~@(var-ize bindings)) ~@body)))
+
 (defmacro with-out-str
   "Evaluates exprs in a context in which *out* is bound to a fresh
   Buffer.  Returns the string created by any nested printing
