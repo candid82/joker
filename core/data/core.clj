5a030920119d178bf8dc275afa1cd293a95693fe
Roman Bataev <roman.bataev@gmail.com>
2016-10-08 12:31:01 -0700

    core: test

--- core/data/core.clj
+++ core/data/core.clj
@@ -2522,6 +2522,16 @@
      `(when-not ~x
         (throw (ex-info (str "Assert failed: " ~message "\n" '~x)))))))
 
+(defn test
+  "test [v] finds fn at key :test in var metadata and calls it,
+  presuming failure will throw exception"
+  {:added "1.0"}
+  [v]
+  (let [f (:test (meta v))]
+    (if f
+      (do (f) :ok)
+      :no-test)))
+
 (defmacro defn-
   "same as defn, yielding non-public def"
   {:added "1.0"}
