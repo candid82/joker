3fe1436880136015b53a98f89a577fe57f3ff81c
Roman Bataev <roman.bataev@gmail.com>
2016-10-09 15:34:35 -0700

    core: tree-seq

--- core/data/core.clj
+++ core/data/core.clj
@@ -2583,6 +2583,22 @@
   [name & decls]
   (list* `defn (with-meta name (assoc (meta name) :private true)) decls))
 
+(defn tree-seq
+  "Returns a lazy sequence of the nodes in a tree, via a depth-first walk.
+  branch? must be a fn of one arg that returns true if passed a node
+  that can have children (but may not).  children must be a fn of one
+  arg that returns a sequence of the children. Will only be called on
+  nodes for which branch? returns true. Root is the root node of the
+  tree."
+  {:added "1.0"}
+  [branch? children root]
+  (let [walk (fn walk [node]
+               (lazy-seq
+                (cons node
+                      (when (branch? node)
+                        (mapcat walk (children node))))))]
+    (walk root)))
+
 (defn empty?
   "Returns true if coll has no items - same as (not (seq coll)).
   Please use the idiom (seq x) rather than (not (empty? x))"
