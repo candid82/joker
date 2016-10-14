9bdfc204cf851e225a7b73bc04ef03c5d1dbd6b5
Roman Bataev <roman.bataev@gmail.com>
2016-10-13 22:01:55 -0700

    core: replace

--- core/data/core.clj
+++ core/data/core.clj
@@ -2666,6 +2666,20 @@
                  xs seen)))]
     (step coll #{})))
 
+(defn replace
+  "Given a map of replacement pairs and a vector/collection, returns a
+  vector/seq with any elements = a key in smap replaced with the
+  corresponding val in smap."
+  {:added "1.0"}
+  [smap coll]
+  (if (vector? coll)
+    (reduce1 (fn [v i]
+               (if-let [e (find smap (nth v i))]
+                 (assoc v i (val e))
+                 v))
+             coll (range (count coll)))
+    (map #(if-let [e (find smap %)] (val e) %) coll)))
+
 (defn empty?
   "Returns true if coll has no items - same as (not (seq coll)).
   Please use the idiom (seq x) rather than (not (empty? x))"
