63a1e1e1eb7596333342d5133a665b4193fee517
Roman Bataev <roman.bataev@gmail.com>
2016-11-02 21:13:03 -0700

    core: memoize

--- core/data/core.clj
+++ core/data/core.clj
@@ -2890,20 +2890,20 @@
        ~@body
        (recur))))
 
-; (defn memoize
-;   "Returns a memoized version of a referentially transparent function. The
-;   memoized version of the function keeps a cache of the mapping from arguments
-;   to results and, when calls with the same arguments are repeated often, has
-;   higher performance at the expense of higher memory use."
-;   {:added "1.0"}
-;   [f]
-;   (let [mem (atom {})]
-;     (fn [& args]
-;       (if-let [e (find @mem args)]
-;         (val e)
-;         (let [ret (apply f args)]
-;           (swap! mem assoc args ret)
-;           ret)))))
+(defn memoize
+  "Returns a memoized version of a referentially transparent function. The
+  memoized version of the function keeps a cache of the mapping from arguments
+  to results and, when calls with the same arguments are repeated often, has
+  higher performance at the expense of higher memory use."
+  {:added "1.0"}
+  [f]
+  (let [mem (atom {})]
+    (fn [& args]
+      (if-let [e (find @mem args)]
+        (val e)
+        (let [ret (apply f args)]
+          (swap! mem assoc args ret)
+          ret)))))
 
 (defn empty?
   "Returns true if coll has no items - same as (not (seq coll)).
