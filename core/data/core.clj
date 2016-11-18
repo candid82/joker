62ad37b70839857ae4402fa9dde70aa733b05656
Roman Bataev <roman.bataev@gmail.com>
2016-11-17 21:25:13 -0800

    core: mapv

--- core/data/core.clj
+++ core/data/core.clj
@@ -3451,6 +3451,22 @@
   [to from]
   (reduce conj to from))
 
+(defn mapv
+  "Returns a vector consisting of the result of applying f to the
+  set of first items of each coll, followed by applying f to the set
+  of second items in each coll, until any one of the colls is
+  exhausted.  Any remaining items in other colls are ignored. Function
+  f should accept number-of-colls arguments."
+  {:added "1.0"}
+  ([f coll]
+   (reduce (fn [v o] (conj v (f o))) [] coll))
+  ([f c1 c2]
+   (into [] (map f c1 c2)))
+  ([f c1 c2 c3]
+   (into [] (map f c1 c2 c3)))
+  ([f c1 c2 c3 & colls]
+   (into [] (apply map f c1 c2 c3 colls))))
+
 (defmacro cond->
   "Takes an expression and a set of test/form pairs. Threads expr (via ->)
   through each form for which the corresponding test
