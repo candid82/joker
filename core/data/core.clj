8247d4d75566e7236732e1b3be304a8cae29d708
Roman Bataev <roman.bataev@gmail.com>
2016-11-17 21:58:37 -0800

    core: frequencies

--- core/data/core.clj
+++ core/data/core.clj
@@ -3522,6 +3522,15 @@
            run (cons fst (take-while #(= fv (f %)) (next s)))]
        (cons run (partition-by f (seq (drop (count run) s))))))))
 
+(defn frequencies
+  "Returns a map from distinct items in coll to the number of times
+  they appear."
+  {:added "1.0"}
+  [coll]
+  (reduce (fn [counts x]
+            (assoc counts x (inc (get counts x 0))))
+          {} coll))
+
 (defmacro cond->
   "Takes an expression and a set of test/form pairs. Threads expr (via ->)
   through each form for which the corresponding test
