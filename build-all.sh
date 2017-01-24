b1c65aa3bf22d85241e4d0f57455a709191daaf6
Roman Bataev <roman.bataev@gmail.com>
2017-01-23 22:43:15 -0800

    Add build-all.sh

---
+++ build-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+version=$1
+
+GOOS=darwin GOARCH=amd64 go build
+zip joker-$version-mac-amd64.zip joker
+GOOS=linux GOARCH=amd64 go build
+zip joker-$version-linux-amd64.zip joker
+GOOS=windows GOARCH=amd64 go build
+zip joker-$version-win-amd64.zip joker
