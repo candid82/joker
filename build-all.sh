00cfac4674bfe0d53701e3608c4cbf4d2c574fc1
Roman Bataev <roman.bataev@gmail.com>
2017-01-23 22:47:12 -0800

    Fix windows build

--- build-all.sh
+++ build-all.sh
@@ -7,4 +7,4 @@ zip joker-$version-mac-amd64.zip joker
 GOOS=linux GOARCH=amd64 go build
 zip joker-$version-linux-amd64.zip joker
 GOOS=windows GOARCH=amd64 go build
-zip joker-$version-win-amd64.zip joker
+zip joker-$version-win-amd64.zip joker.exe
