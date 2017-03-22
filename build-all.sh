8cd82a9b2c1d4ec898bc931788552c410d205883
Roman Bataev <roman.bataev@gmail.com>
2017-03-21 21:48:24 -0700

    Improve build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,11 @@
 
 version=$1
 
+if [ -z "$version" ]; then
+  echo "Usage: build-all.sh <version>"
+  exit
+fi
+
 GOOS=darwin GOARCH=amd64 go build
 zip joker-$version-mac-amd64.zip joker
 GOOS=linux GOARCH=amd64 go build
