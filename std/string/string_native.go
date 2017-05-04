9f5d8bd7eb442c57d33dd6ba826f66fedf890c55
Roman Bataev <roman.bataev@gmail.com>
2017-05-03 22:05:26 -0700

    joker.string/reverse

--- std/string/string_native.go
+++ std/string/string_native.go
@@ -167,6 +167,14 @@ func replaceFirst(s string, match Object, repl string) string {
 	}
 }
 
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func init() {
 	newLine, _ = regexp.Compile("\r?\n")
 }
