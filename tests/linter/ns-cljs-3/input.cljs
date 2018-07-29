e20517b85a84149938d03a261d0311fb96313c1f
Roman Bataev <roman@bataev.me>
2018-07-28 21:11:07 -0700

    Add support for shadow-cljs's :default option in require

--- tests/linter/ns-cljs-3/input.cljs
+++ tests/linter/ns-cljs-3/input.cljs
@@ -1,9 +1,11 @@
 (ns example.macros
   (:require-macros [cljs.core.async.macros :as async])
   (:require [cljs.core.async :as async]
-            [left-pad :as lp]))
+            [left-pad :as lp]
+            ["material-ui/RaisedButton" :default RaisedButton]))
 
 (async/t)
 (left-pad)
 (lp 1)
+RaisedButton
 (t)
