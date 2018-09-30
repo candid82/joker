4626c5ff3c4a9286894ef71b308cae7eab1960f8
Roman Bataev <roman@bataev.me>
2018-09-30 12:35:22 -0700

    Pass -all to go vet.
    Otherwise -shadow=true makes it only perform that particular check.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go generate ./... && go tool vet -shift=false -shadow=true ./ && go build && ./joker "$@"
+go generate ./... && go tool vet -all -shadow=true ./ && go build && ./joker "$@"
