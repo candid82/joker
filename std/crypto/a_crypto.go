af4fa8ae2a1f126303defb5adf0bb9fb8304011c
Roman Bataev <roman@bataev.me>
2019-04-08 21:46:40 -0700

    Implement joker.hex/encode-string, joker.hex/decode-string, joker.crypto/sha256.

--- std/crypto/a_crypto.go
+++ std/crypto/a_crypto.go
@@ -4,6 +4,7 @@ package crypto
 
 import (
 	. "github.com/candid82/joker/core"
+	"crypto/sha256"
 )
 
 var cryptoNamespace = GLOBAL_ENV.EnsureNamespace(MakeSymbol("joker.crypto"))
@@ -24,6 +25,21 @@ var hmac_ Proc = func(_args []Object) Object {
 	return NIL
 }
 
+var sha256_ Proc = func(_args []Object) Object {
+	_c := len(_args)
+	switch {
+	case _c == 1:
+		data := ExtractString(_args, 0)
+		 t := sha256.Sum256([]byte(data))
+		_res := string(t[:])
+		return MakeString(_res)
+
+	default:
+		PanicArity(_c)
+	}
+	return NIL
+}
+
 func init() {
 
 	cryptoNamespace.ResetMeta(MakeMeta(nil, "Implements common cryptographic and hash functions.", "1.0"))
@@ -33,5 +49,10 @@ func init() {
 			NewListFrom(NewVectorFrom(MakeSymbol("algorithm"), MakeSymbol("message"), MakeSymbol("key"))),
 			`Returns HMAC signature for message and key using specified algorithm.
   Algorithm is one of the following: :sha1, :sha224, :sha256, :sha384, :sha512.`, "1.0"))
+
+	cryptoNamespace.InternVar("sha256", sha256_,
+		MakeMeta(
+			NewListFrom(NewVectorFrom(MakeSymbol("data"))),
+			`Returns the SHA256 checksum of the data.`, "1.0"))
 
 }
