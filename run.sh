f9770196d26dfc403c850cff777672dd9f443fd9
James Craig Burley <430319+jcburley@users.noreply.github.com>
2019-08-23 11:24:10 -0400

    For cross-platform builds, stop after building Joker the 1st time

--- run.sh
+++ run.sh
@@ -15,13 +15,16 @@ if [ "$1" == "-v" ]; then
   ./joker -e '(print "\nLibraries available in this build:\n  ") (loaded-libs) (println)'
 fi
 
-SUM256="$(GOOS="$GOHOSTOS" GOARCH="$GOHOSTARCH" go run tools/sum256dir/main.go std)"
-(cd std; ../joker generate-std.joke 2> /dev/null)
-NEW_SUM256="$(GOOS="$GOHOSTOS" GOARCH="$GOHOSTARCH" go run tools/sum256dir/main.go std)"
+# Check for changes in std, and run just-built Joker, only when building for host os/architecture.
+if [ "$GOOS" = "$GOHOSTOS" -a "$GOARCH" = "$GOHOSTARCH" ]; then
+    SUM256="$(GOOS="$GOHOSTOS" GOARCH="$GOHOSTARCH" go run tools/sum256dir/main.go std)"
+    (cd std; ../joker generate-std.joke 2> /dev/null)
+    NEW_SUM256="$(GOOS="$GOHOSTOS" GOARCH="$GOHOSTARCH" go run tools/sum256dir/main.go std)"
 
-if [ "$SUM256" != "$NEW_SUM256" ]; then
-  echo 'std has changed, rebuilding...'
-  build
-fi
+    if [ "$SUM256" != "$NEW_SUM256" ]; then
+        echo 'std has changed, rebuilding...'
+        build
+    fi
 
 ./joker "$@"
+fi
