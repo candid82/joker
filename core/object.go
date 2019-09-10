c5e468bdd8a71c269f5e33a94f31804e35867ade
Roman Bataev <roman@bataev.me>
2019-09-09 22:14:01 -0700

    Improve ToString for Double.

--- core/object.go
+++ core/object.go
@@ -1112,7 +1112,7 @@ func MakeDouble(d float64) Double {
 }
 
 func (d Double) ToString(escape bool) string {
-	return fmt.Sprintf("%f", d.D)
+	return fmt.Sprintf("%g", d.D)
 }
 
 func (d Double) Equals(other interface{}) bool {
