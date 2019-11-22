a986cd4fe9c529c0eae2e4dd22b577f06c5ca013
James Craig Burley <430319+jcburley@users.noreply.github.com>
2019-11-22 03:12:52 -0500

    Speed up builds and time them

--- core/build.sh
+++ core/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-rm -fv a_*_{code,data}.go a_code.go
-go run gen_code/gen_code.go || exit $?
-go run gen_data/gen_data.go || exit $?
+set -x
 
-go fmt a_*.go
-cd ..; go build
+rm -fv a_*_{code,data}.go a_code.go
+
+# Build gen_data before generating code that would otherwise be
+# unnecessarily compiled into it.
+time go build -o gen_data/gen_data gen_data/gen_data.go
+
+time go run gen_code/gen_code.go || exit $?
+
+time ./gen_data/gen_data || exit $?
+
+time go fmt a_*.go
+
+cd ..; time go build
