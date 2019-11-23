92312e09d3c5f2df3c910c2d422f4fe8d7650574
James Craig Burley <430319+jcburley@users.noreply.github.com>
2019-11-23 00:38:44 -0500

    [WiP] Use the same 'time' command throughout

--- core/build.sh
+++ core/build.sh
@@ -8,16 +8,18 @@ mv -iv a_*.go "$NOW" || :
 [ -x ../joker ] && cp -aiv ../joker "$NOW"
 (git log -n 1; git status) > "$NOW/git.txt"
 
+time=$(which time)
+
 set -x  # Echo commands
 
 # Build gen_data before generating code that would otherwise be
 # unnecessarily compiled into it.
-time go build -o gen_data/gen_data gen_data/gen_data.go
+$time go build -o gen_data/gen_data gen_data/gen_data.go
 
-time go run gen_code/gen_code.go
+$time go run gen_code/gen_code.go
 
-time ./gen_data/gen_data
+$time ./gen_data/gen_data
 
-time go fmt a_*.go
+$time go fmt a_*.go
 
-cd ..; KEEP_A_FILES=true time ./run.sh "$@"
+cd ..; KEEP_A_FILES=true $time ./run.sh "$@"
