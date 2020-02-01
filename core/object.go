89d0c37d88a7626722071a7bf3ff8fc8aebd994f
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-02-01 14:19:52 -0500

    Exclude generated code from builds of code-generating tools
    This avoids a chicken-and-egg problem that comes up when e.g. changing whether a Joker type is a reference type.
    
    It seems better to just avoid the problem, by not including code being generated via `go generate ./...` when building the tools doing the generation, than to document how to work around it.

--- core/object.go
+++ core/object.go
@@ -1,6 +1,6 @@
-//go:generate go run gen/gen_types.go assert Comparable *Vector Char String Symbol Keyword Regex Boolean Time Number Seqable Callable *Type Meta Int Double Stack Map Set Associative Reversible Named Comparator *Ratio *Namespace *Var Error *Fn Deref *Atom Ref KVReduce Pending *File io.Reader io.Writer StringReader io.RuneReader *Channel
-//go:generate go run gen/gen_types.go info *List *ArrayMapSeq *ArrayMap *HashMap *ExInfo *Fn *Var Nil *Ratio *BigInt *BigFloat Char Double Int Boolean Time Keyword Regex Symbol String *LazySeq *MappingSeq *ArraySeq *ConsSeq *NodeSeq *ArrayNodeSeq *MapSet *Vector *VectorSeq *VectorRSeq
-//go:generate go run gen_data/gen_data.go
+//go:generate go run -tags gen_types,gen_data gen/gen_types.go assert Comparable *Vector Char String Symbol Keyword Regex Boolean Time Number Seqable Callable *Type Meta Int Double Stack Map Set Associative Reversible Named Comparator *Ratio *Namespace *Var Error *Fn Deref *Atom Ref KVReduce Pending *File io.Reader io.Writer StringReader io.RuneReader *Channel
+//go:generate go run -tags gen_types,gen_data gen/gen_types.go info *List *ArrayMapSeq *ArrayMap *HashMap *ExInfo *Fn *Var Nil *Ratio *BigInt *BigFloat Char Double Int Boolean Time Keyword Regex Symbol String *LazySeq *MappingSeq *ArraySeq *ConsSeq *NodeSeq *ArrayNodeSeq *MapSet *Vector *VectorSeq *VectorRSeq
+//go:generate go run -tags gen_data gen_data/gen_data.go
 
 package core
 
