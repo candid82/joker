2cff6ad32af570c318b795cb20896224ed0af44c
James Craig Burley <430319+jcburley@users.noreply.github.com>
2020-02-15 00:01:32 -0500

    Split value-sorting code into its own source file with suitable callbacks

--- core/gen_go/unsafe_stuff.go
+++ core/gen_go/unsafe_stuff.go
@@ -1,16 +1,14 @@
 package gen_go
 
+// NOTE: This code comes from github.com/jcburley/go-spew (originally davecgh/go-spew):
+
 import (
 	"reflect"
 	"unsafe"
 )
 
-// NOTE: Below this line, code comes from github.com/jcburley/go-spew:
-
-// This comes from (davecgh|jcburley)/go-spew/bypass.go.
 const flagPrivate = 0x20
 
-// This comes from (davecgh|jcburley)/go-spew/bypass.go.
 var flagValOffset = func() uintptr {
 	field, ok := reflect.TypeOf(reflect.Value{}).FieldByName("flag")
 	if !ok {
@@ -19,15 +17,12 @@ var flagValOffset = func() uintptr {
 	return field.Offset
 }()
 
-// This comes from (davecgh|jcburley)/go-spew/bypass.go.
 type flag uintptr
 
-// This comes from (davecgh|jcburley)/go-spew/bypass.go.
 func flagField(v *reflect.Value) *flag {
 	return (*flag)(unsafe.Pointer(uintptr(unsafe.Pointer(v)) + flagValOffset))
 }
 
-// This comes from (davecgh|jcburley)/go-spew/bypass.go.
 func UnsafeReflectValue(v reflect.Value) reflect.Value {
 	if !v.IsValid() || (v.CanInterface() && v.CanAddr()) {
 		return v
