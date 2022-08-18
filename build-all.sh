aa3c65d9c81d6991d5c25d38206ab599dda5fa7a
Roman Bataev <roman@bataev.me>
2022-08-17 23:29:32 -0400

    Drop version from release file names.
    https://github.com/candid82/joker/issues/425

--- build-all.sh
+++ build-all.sh
@@ -2,14 +2,9 @@
 
 version=$1
 
-if [ -z "$version" ]; then
-  echo "Usage: build-all.sh <version>"
-  exit
-fi
-
 GOOS=darwin GOARCH=amd64 go build
-zip joker-$version-mac-amd64.zip joker
+zip joker-mac-amd64.zip joker
 GOOS=linux GOARCH=amd64 go build
-zip joker-$version-linux-amd64.zip joker
+zip joker-linux-amd64.zip joker
 GOOS=windows GOARCH=amd64 go build
-zip joker-$version-win-amd64.zip joker.exe
+zip joker-win-amd64.zip joker.exe
